b48004da2a0c6e9d4cc716227e5193e5185f9b40
JosuePimentel <josue.farias.pimentel@gmail.com>
2024-01-25 19:22:16 -0300

    new commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+let boxInvite = document.querySelector('.box-invite')
+
+boxInvite.addEventListener('click', e => {
+    e.preventDefault()
+    
+    boxInvite.classList.add('clicked')
+
+    setInterval(() => {
+        boxInvite.style.display = 'none'
+        document.querySelector('span.loader').style.display = 'block'
+        setInterval(() => {
+            window.location.href = 'sources/invite.html'
+        }, 1000)
+    }, 4500)
+})
+
